93fc01e8d21d7d547d65eb0fa3ef443305fdc409
chetanachaudhari24 <chetana.chaudhari24@gmail.com>
2021-12-26 18:50:18 +0530

    added pipeline code

---
+++ pipeline.groovy
@@ -0,0 +1,41 @@
+pipeline {
+    agent any
+
+
+    triggers {
+        pollSCM('* * * * *')
+    }
+
+
+    stages {
+        stage('Checkout'){
+            steps {
+                 git branch: 'main', url: 'https://github.com/chetanachaudhari24/jgsu-spring-petclinic.git'
+            }
+
+        }
+
+        stage('Build') {
+            steps {
+                // Get some code from a GitHub repository
+                // git branch: 'main', url: 'https://github.com/g0t4/jgsu-spring-petclinic.git'
+
+                sh './mvnw clean package'
+                // Run Maven on a Unix agent.
+                // sh "mvn -Dmaven.test.failure.ignore=true clean package"
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                always {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
